dd7e57401558c76935e655841dbb6bd2e12c282b
yeniugo <hukaihope@gmail.com>
2024-06-17 11:42:11 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NavigationStack",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
         .macOS(.v10_15),
         .watchOS(.v6)
     ],
